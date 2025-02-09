90ba7efd8a55fbceeb4bd7d01d22a8d927992fd6
laiozago <laiozago@gmail.com>
2025-02-09 05:18:13 -0300

    adiciona ícone ao cabeçalho e atualiza lista de URLs a serem armazenados no service worker

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,5 +1,30 @@
 const CACHE_NAME = "hiit-timer-cache-v1";
-const urlsToCache = [];
+const urlsToCache = [
+    "/",                    // Página inicial
+    "/index.html",          // Página principal
+    "/manifest.json",       // Manifest do PWA
+
+    // Páginas internas
+    "/pages/hiit.html",
+    "/pages/results.html",
+    "/pages/setHiit.html",
+
+    // CSS
+    "/css/reset.css",
+    "/css/results.css",
+    "/css/setHiit.css",
+    "/css/styles.css",
+
+    // JavaScript
+    "/scripts/app.js",
+    "/scripts/results.js",
+    "/scripts/service-worker.js",
+    "/scripts/setHiit.js",
+
+    // Ícones do PWA
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png"
+];
 
 self.addEventListener("install", event => {
     event.waitUntil(
